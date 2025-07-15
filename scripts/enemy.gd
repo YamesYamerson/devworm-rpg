7a49c9ea890f55c1ad2056f286e7f2c5ef155b54
James McLean <jamesrmclean88@gmail.com>
2025-07-14 23:54:06 -0300

    feat: combat
    Added damage, cooldown timer, working on second half of tutorial

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 var speed = 80
-var player_chase = false
+var health = 100
 var player = null
+var player_chase = false
+var player_inattack_zone = false
 
 func _physics_process(delta: float) -> void:
+	handle_damage()
+	
 	if player_chase:
 		position += (player.position - position) / speed
 		
@@ -22,7 +26,24 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
 	player_chase = true
 
-
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+
+func handle_damage():
+	if player_inattack_zone && global.player_current_attack == true:
+		health = health -20
+		print("enemy health = ", health)
+		if health <= 0:
+			self.queue_free()
+
+func enemy():
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+
 const speed = 100;
 var current_dir = "none"
 
@@ -8,6 +13,13 @@ func _ready():
 
 func _physics_process(delta):
 	player_movement(delta)
+	enemy_attack()
+	
+	if health <= 0:
+		player_alive = false #todo - goto menu
+		health = 0
+		print("player has been killed...")
+		self.queue_free()
 
 func player_movement(delta):
 	if Input.is_action_pressed("ui_right"):
@@ -68,7 +80,27 @@ func play_anim(movement):
 			anim.play("back_walk")
 		elif(movement == 0):
 			anim.play("back_idle")		
-	
-		
-	
-		
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = true
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
+
+func enemy_attack():
+	if enemy_inattack_range && enemy_attack_cooldown == true:
+		health = health -20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true
+
+
+
+func player():
+	pass
